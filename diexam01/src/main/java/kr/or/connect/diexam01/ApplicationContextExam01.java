a7bca2a8ab92dcb0af76413d724972dbb8165085
yonghyun123 <model9960@naver.com>
2020-11-12 20:32:35 +0900

    add gitignore and readme

---
+++ diexam01/src/main/java/kr/or/connect/diexam01/ApplicationContextExam01.java
@@ -0,0 +1,22 @@
+package kr.or.connect.diexam01;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class ApplicationContextExam01 {
+
+	public static void main(String[] args) {
+		ApplicationContext ac = new ClassPathXmlApplicationContext("classpath:ApplicationContext.xml");
+		System.out.println("초기화완료!!");
+		
+		UserBean userBean = (UserBean)ac.getBean("userBean");
+		userBean.setName("yong");
+		
+		System.out.println(userBean.getName());
+		
+		UserBean userBean2 = (UserBean)ac.getBean("userBean");
+		
+		if(userBean == userBean2)
+			System.out.println("같은 인스턴스입니다.singleton pattern");
+	}
+}
