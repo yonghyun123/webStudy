049e789014ece0a866aa01497b1b0b920bf49df7
yonghyun123 <model9960@naver.com>
2020-11-14 19:17:09 +0900

    How to use SpringJDBC (CRUD)

--- daoexam/src/main/java/kr/or/connect/daoexam/config/ApplicationConfig.java
+++ daoexam/src/main/java/kr/or/connect/daoexam/config/ApplicationConfig.java
@@ -1,10 +1,11 @@
 package kr.or.connect.daoexam.config;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 
 @Configuration
 @Import({DBConfig.class})
-
+@ComponentScan(basePackages = { "kr.or.connect.daoexam.dao"})
 public class ApplicationConfig {
 
 }
